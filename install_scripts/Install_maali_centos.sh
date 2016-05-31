99a502689b2e788413ec321c63c855546ac3789c
chrisbpawsey <chris.bording@pawsey.org.au>
2016-05-31 03:05:17 +0000

    Install scripts for centos and fedora packages

---
+++ install_scripts/Install_maali_centos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# update your current image from the repo 
+# This is will ensure that the yum installs will work!
+sudo yum update
+
+# install to allow x11 forwarding of gui interfaces
+sudo yum -y install xorg-x11-xauth xorg-x11-apps 
+
+# install nano xemacs emacs editors
+sudo yum -y install nano xemacs xemacs-el xemacs-devel xemacs-packages-base xemacs-packages-extra
+sudo yum -y install emacs emacs-common emacs-goodies-el 
+
+# install tools that are needed for maali to work
+sudo yum -y install bc wget make environment-modules
+
+# install the default gnu compilers
+# to find the the version number "gcc -V"
+sudo yum -y install gcc gcc-gfortran gcc-c++ 
+
+
+
+
+
--- install_scripts/Install_maali_fedora.sh
+++ install_scripts/Install_maali_fedora.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# update your current image from the repo 
+# This is will ensure that the remaining package management installs will work!
 sudo dnf update
 
+# install to allow x11 fowarding of gui interfaces
 sudo dnf -y install xorg-x11-xauth xorg-x11-apps 
+
+# install nano xemacs emacs editors
 sudo dnf -y install nano xemacs xemacs-el xemacs-devel xemacs-packages-base xemacs-packages-extra
 sudo dnf -y install emacs emacs-common emacs-goodies-el 
-sudo dnf -y install bc wget make 
+
+# install tools that are needed for maali to work
+sudo dnf -y install bc wget make environment-modules
+
+# install the default gnu compilers
+# to find the the version number "gcc -V"
 sudo dnf -y install gcc gcc-gfortran gcc-c++ 
-sudo dnf -y install environment-modules
+
 
 
 
