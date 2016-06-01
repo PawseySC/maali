d7035759ae290c5917c621051f63a3442d4138e2
Chris Bording <chris.bording@pawsey.org.au>
2016-06-01 02:40:43 +0000

    added ubuntu install script

---
+++ install_scripts/Install_maali_ubuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# update your current image from the default repo 
+# This is will ensure that the remaining package management installs will work!
+sudo apt-get -y update
+
+# install to allow x11 fowarding of gui interfaces
+sudo apt-get -y install xauth x11-apps 
+
+# install nano xemacs emacs editors
+sudo apt-get -y install nano xemacs21 xemacs21-bin xemacs21-supportel xemacs21-basesupport-el
+
+sudo apt-get -y install emacs emacs24 emacs24-common emacs-goodies-el 
+
+# install tools that are needed for maali to work
+sudo apt-get -y install bc wget make environment-modules
+
+# install the default gnu compilers
+# to find the the version number "gcc -v"
+sudo apt-get -y install gcc-4.8 gfortran-4.8 g++-4.8 libgomp1
+
+
+if [ -d "/nectar" ]; then
+  sudo mkdir /nectar  
+  sudo chown -R debian /nectar
+fi
+ 
