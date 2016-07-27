d6eae42c691c1772ac499e35126da1de4b2e4e45
Ubuntu <ubuntu@parallel.novalocal>
2016-07-27 04:17:41 +0000

    add install_bioc.sh

---
+++ install_scripts/install_bioc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get -y install r-base r-base-dev
+sudo apt-get -y install r-bioc-affy
+sudo apt-get -y install r-bioc-affyio
+sudo apt-get -y install r-bioc-altcdfenvs
+sudo apt-get -y install r-bioc-annotate
+sudo apt-get -y install r-bioc-annotationdbi
+sudo apt-get -y install r-bioc-aroma.light
+sudo apt-get -y install r-bioc-biobase
+sudo apt-get -y install r-bioc-biocgenerics
+sudo apt-get -y install r-bioc-biocinstaller
+sudo apt-get -y install r-bioc-biocparallel
+sudo apt-get -y install r-bioc-biomart
+sudo apt-get -y install r-bioc-biostrings
