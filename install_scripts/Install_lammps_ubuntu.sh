62a58997d274058aa126cc58dc9273aee486928d
Ubuntu <ubuntu@lammps-dev.novalocal>
2016-12-15 14:25:58 +0000

    added require package to Install_lammps_ubuntu.sh

--- install_scripts/Install_lammps_ubuntu.sh
+++ install_scripts/Install_lammps_ubuntu.sh
@@ -1,7 +1,13 @@
 #!/bin/bash 
 
+#additional packages to install
+sudo apt install -y libjpeg-dev 
+sudo apt install -y libpng12-dev
+sudo apt install -y python2.7-dev
+sudo apt install -y python-dev
+
 # to install gromacs/5.1.4
-# need to install these depende applications/libraries 
+# need to install these dependent applications/libraries 
 # lapack/3.6.1 openmpi/2.0.1 fftw-parallel/3.3.5  
 # assumes maali/1.0.6 is install
 
@@ -13,7 +19,8 @@ maali -t lapack -v 3.6.1 -c nectar -w
 
 maali -t openmpi -v 2.0.1 -c nectar -w
 
-maali -t fftw-paralllel -v 3.3.5 -c nectar -w
+maali -t fftw-parallel -v 3.3.5 -c nectar -w
 
 maali -t lammps -v 18Oct16 -c nectar -w
+
 
