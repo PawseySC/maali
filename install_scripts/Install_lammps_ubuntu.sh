eebc330f0b88a26a0d66d217a14c374dc9f6c150
Ubuntu <ubuntu@lammp-dev2.novalocal>
2016-12-22 03:25:48 +0000

    Installed using apt ffmpeg and comments one lammps code dependecy packages

--- install_scripts/Install_lammps_ubuntu.sh
+++ install_scripts/Install_lammps_ubuntu.sh
@@ -4,13 +4,16 @@
 sudo chown -R ubuntu /mnt
 
 #additional packages to install
+sudo apt install -y ffmpeg
 sudo apt install -y libjpeg-dev 
 sudo apt install -y libpng12-dev
 sudo apt install -y python2.7-dev
 sudo apt install -y python-dev
 
-# to install gromacs/5.1.4
+# to install lammps 
 # need to install these dependent applications/libraries 
+# cmake is used to install lapack 
+# lapack is needed for fftw-parallel
 # lapack/3.6.1 openmpi/2.0.1 fftw-parallel/3.3.5  
 # assumes maali/1.0.6 is install
 
